750ebb479db1f6b365db6dcbf459ee78e50c6992
nkaaf <nkaaf@protonmail.com>
2026-05-08 18:43:11 +0000

    Fix package installation check

--- test/latex-texlive/common.sh
+++ test/latex-texlive/common.sh
@@ -12,7 +12,7 @@ texlive_version() {
 }
 
 tlmgr_package_installed() {
-    installed=$(tlmgr info "$1" | grep installed | cut --delimiter : --fields 2 | xargs)
+    installed=$(tlmgr info "$1" | grep "installed:" | cut --delimiter : --fields 2 | xargs)
     [ "$installed" = "Yes" ]
 }
 
