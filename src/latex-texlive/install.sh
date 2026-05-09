c54e241815f2d68239d7dc63de8cbac7bdad60ee
Niklas Kaaf <nkaaf@protonmail.com>
2026-05-09 13:41:46 +0200

    LaTeX TeX Live - Add Tests & Fix historic releases (#8)
    Add Tests for 2020-2026

--- src/latex-texlive/install.sh
+++ src/latex-texlive/install.sh
@@ -2,8 +2,15 @@
 
 set -eu
 
+. ./common.sh
+
 if [ "$(id -u)" -ne 0 ]; then
     echo 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+if [ "$SCHEME" = "bookpub" ] && ! year_min 2022; then
+    echo "Current selected year is '$YEAR', but the scheme 'bookpub' exists since 2022."
     exit 1
 fi
 
---
+++ test/latex-texlive/test_2024_minimal_package_multi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+. ./dev-container-features-test-lib
+. ./common.sh
+
+check "TeX Live installed" texlive_installed
+check "TeX Live version" texlive_version "2024"
+check "TeX Live scheme" tlmgr_package_installed "scheme-minimal"
+check "tlmgr package xifthen installed" tlmgr_package_installed "xifthen"
+check "tlmgr package ifmtarg installed" tlmgr_package_installed "ifmtarg"
+check "Documentation not installed" docfiles_not_installed
+check "Source not installed" srcfiles_not_installed
+
+reportResults
