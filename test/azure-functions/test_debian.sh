eeb9d2c45d354f81f03ea24f83297507ef893534
nkaaf <nkaaf@protonmail.com>
2026-08-21 13:44:36 +0000

    fix Tests

--- test/azure-functions/test_debian.sh
+++ test/azure-functions/test_debian.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 . ./dev-container-features-test-lib
 . ./common.sh
 
-check "azure-functions-core-tools-v4 installed" dpkg_package_installed azure-functions-core-tools-v4
+check "azure-functions-core-tools-4 installed" dpkg_package_installed azure-functions-core-tools-4
 
 reportResults
